a8d85ee44e0dda58376e6d5fc3a3ff3ce0f66a57
Marionjames-ally <marionjames2018@gmail.com>
2020-01-17 12:08:25 +0300

    design the application structure by generating classess and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayComponent } from './display/display.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { ServiceComponent } from './service/service.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayComponent,
+    RepositoryComponent,
+    ServiceComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
